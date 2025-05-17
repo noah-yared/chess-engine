85affe581b71a5105d39eeccd3bdd7ce5f0f49f8
Noah Yared <noahy8105@gmail.com>
2025-05-17 15:13:50 -0400

    updated test functions for new board ctor and moved helpers to utils

--- tests/boards/pieceBoards/tests.hpp
+++ tests/boards/pieceBoards/tests.hpp
@@ -28,24 +28,24 @@
 #include "evalBoards.hpp"
 #include "evaluate.h"
 
-using ull = unsigned long long;
-using Moves = std::vector<std::unique_ptr<Move>>;
-using Tile = std::pair<int, int>;
+// using ull = unsigned long long;
+// using Moves = std::vector<std::unique_ptr<Move>>;
+// using Tile = std::pair<int, int>;
 
 // Function declarations
-void printBitmap(ull bitmap);
-void printAttacks(std::array<ull, 64> attacks);
-ull* toBitboard(std::array<std::array<char, 8>, 8>& stringifiedBoard, ull *bbs);
-void printStringifiedBoard(std::array<std::array<char, 8>, 8>& board);
-std::string stringify(Tile tile);
-std::string stringify(int tile);
-std::string stringify(const std::unique_ptr<Move>& move);
-std::string stringify(std::pair<Tile, Tile> move);
-void printMove(const std::unique_ptr<Move>& move);
-void printMoves(std::vector<std::pair<Tile, Tile>>& moves);
-void printMoves(Moves& moves);
-std::string hashMove(std::pair<Tile, Tile> move);
-std::string hashMove(std::unique_ptr<Move>& move);
+// void printBitmap(ull bitmap);
+// void printAttacks(std::array<ull, 64> attacks);
+// ull* toBitboard(std::array<std::array<char, 8>, 8>& stringifiedBoard, ull *bbs);
+// void printStringifiedBoard(std::array<std::array<char, 8>, 8>& board);
+// std::string stringify(Tile tile);
+// std::string stringify(int tile);
+// std::string stringify(const std::unique_ptr<Move>& move);
+// std::string stringify(std::pair<Tile, Tile> move);
+// void printMove(const std::unique_ptr<Move>& move);
+// void printMoves(std::vector<std::pair<Tile, Tile>>& moves);
+// void printMoves(Moves& moves);
+// std::string hashMove(std::pair<Tile, Tile> move);
+// std::string hashMove(std::unique_ptr<Move>& move);
 bool matches(Moves& result, std::vector<std::pair<Tile, Tile>> expected);
 
 template<std::size_t N>
@@ -57,23 +57,13 @@ void testMoveGeneration(std::function<Moves(Board*, Side)> generatingFunction,
                         std::array<bool, N> testingBlack,
                         std::array<std::optional<int>, N> enpassantWhite,
                         std::array<std::optional<int>, N> enpassantBlack,
-                        std::array<std::vector<char>, N> castlingPrivileges,
-                        std::array<int, N> bKing,
-                        std::array<int, N> wKing,
-                        std::array<bool, N> isWhiteKingInCheck,
-                        std::array<bool, N> isBlackKingInCheck,
-                        std::array<ull, N> whiteBB,
-                        std::array<ull, N> blackBB);
+                        std::array<std::vector<char>, N> castlingPrivileges);
 
 template<std::size_t N>
 void testEvaluator(std::function<Evaluation::Score(const Board&)> evaluatingFunction,
                     std::array<ChessBoard, N> testBoards,
                     std::array<std::optional<int>, N> enpassant,
-                    std::array<std::vector<char>, N> castlingPrivileges,
-                    std::array<bool, N> kingInCheckWhite,
-                    std::array<bool, N> kingInCheckBlack,
-                    std::array<ull, N> whiteAttackers,
-                    std::array<ull, N> blackAttackers);
+                    std::array<std::vector<char>, N> castlingPrivileges);
 
 void testAttackGeneration();
 void testKingMoveGeneration();
@@ -83,8 +73,5 @@ void testKnightMoveGeneration();
 void testQueenMoveGeneration();
 void testRookMoveGeneration();
 void testEvaluation();
-
-// Global variables
-extern std::unordered_map<char, int> pieceIndex;
 
 #endif // TESTS_HPP
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,120 +1,17 @@
 #include "tests.hpp"
+#include "utils.h"
 #include "knights.h"
 #include "sliding.h"
 #include "evalBoards.hpp"
 #include "evaluate.h"
 
-
-using ull = unsigned long long;
-using Moves = std::vector<std::unique_ptr<Move>>;
-using Tile = std::pair<int, int>;
-
-
-std::unordered_map<char, int> pieceIndex { /* map pieces to bb index*/
-  {'p', 0}, {'P', 6},
-  {'r', 1}, {'R', 7},
-  {'n', 2}, {'N', 8},
-  {'b', 3}, {'B', 9},
-  {'q', 4}, {'Q', 10},
-  {'k', 5}, {'K', 11}
-};
-
-void printBitmap(ull bitmap) {
-  ull mask = 0xFFULL;
-  for (int row = 0; row < 8; row++)
-    std::cout << std::bitset<8>((bitmap >> (56-8*row)) & mask).to_string() << '\n';
-}
-
-void printAttacks(std::array<ull, 64> attacks) {
-  for (ull bm : attacks) { printBitmap(bm); std::cout << '\n'; }
-}  
-
-ull* toBitboard(std::array<std::array<char, 8>, 8>& stringifiedBoard, ull *bbs) {
-  ull *bb = bbs, mask = 1ULL; 
-  for (auto outer_rit = stringifiedBoard.rbegin(); outer_rit != stringifiedBoard.rend(); ++outer_rit) {
-    for (auto rit = outer_rit->rbegin(); rit != outer_rit->rend(); ++rit) {
-      if (*rit != '.') bbs[pieceIndex[*rit]] |= mask; /* piece at square */
-      mask <<= 1;
-    }
-  }
-  return bbs;
-}
-
-void printStringifiedBoard(std::array<std::array<char, 8>, 8>& board) {
-  for (auto row : board) {
-    for (char c : row) std::cout << c;
-    std::cout << '\n';
-  }
-}
-
-void printBoard(const Board& board) {
-  std::array<std::array<char, 8>, 8> stringifiedBoard; 
-  std::array<char, 8> defaultRow; defaultRow.fill('.');
-  stringifiedBoard.fill(defaultRow);
-  for (auto it = pieceIndex.begin(); it != pieceIndex.end(); it++) {
-    std::string bitstring = std::bitset<64>(board.readBB(static_cast<Pieces::piece>(it->second))).to_string();
-    for (int i = 0; i < 64; i++) {
-      if (bitstring[i] == '1') {
-        stringifiedBoard[7 - (i / 8)][i % 8] = it->first;
-      }
-    }
-  }
-  printStringifiedBoard(stringifiedBoard);
-}
-
-std::string stringify(Tile tile) {
-  std::stringstream ss;
-  ss << '(' << tile.first << ", " << tile.second << ')';
-  return ss.str();
-}
-
-std::string stringify(int tile) {
-  std::stringstream ss;
-  ss << '(' << (tile / 8) << ", " << (7 - (tile % 8)) << ')';
-  return ss.str();
-}
-
-std::string stringify(const std::unique_ptr<Move>& move) {
-  std::stringstream ss;
-  ss << '(' << stringify(move->i()) << ", " << stringify(move->f()) <<')';
-  return ss.str();
-}
-
-std::string stringify(std::pair<Tile, Tile> move) {
-  std::stringstream ss;
-  ss << '(' << stringify(move.first) << ", " << stringify(move.second) << ") ";
-  return ss.str();
-}
-
-void printMove(const std::unique_ptr<Move>& move) {
-  if (move)
-    std::cout << stringify(move) << std::endl;
-  else
-    std::cerr << "Move pointer is null!" << std::endl;
-}
-
-void printMoves(Moves& moves) {
-  for (auto& move : moves)
-    std::cout << stringify(move) << std::endl;
-}
-
-void printMoves(std::vector<std::pair<Tile, Tile>>& moves)  {
-  for (auto& move : moves)
-    std::cout << stringify(move) << std::endl;
-}
-
-std::string hashMove(std::pair<Tile, Tile> move) {
-  std::stringstream ss;
-  Tile tile1 = move.first, tile2 = move.second;
-  ss << tile1.first << tile1.second << tile2.first << tile2.second;
-  return ss.str();
-}
-
-std::string hashMove(std::unique_ptr<Move>& move) {
-  std::stringstream ss;
-  ss << (move->i() / 8) << (7 - move->i() % 8) << (move->f() / 8) << (7 - move->f() % 8);
-  return ss.str(); 
-}
+#include <vector>
+#include <unordered_set>
+#include <string>
+#include <array>
+#include <functional>
+#include <optional>
+#include <sstream>
 
 bool matches(Moves& result, std::vector<std::pair<Tile, Tile>> expected) {
   if (result.size() != expected.size()) /* mismatching number of moves generated */
@@ -137,16 +34,10 @@ void testMoveGeneration(std::function<Moves(Board*, Side)> generatingFunction,
                         std::array<bool, N> testingBlack,
                         std::array<std::optional<int>, N> enpassantWhite,
                         std::array<std::optional<int>, N> enpassantBlack,
-                        std::array<std::vector<char>, N> castlingPrivileges,
-                        std::array<bool, N> kingInCheckWhite,
-                        std::array<bool, N> kingInCheckBlack,
-                        std::array<ull, N> whiteAttackers,
-                        std::array<ull, N> blackAttackers) {
+                        std::array<std::vector<char>, N> castlingPrivileges) {
   int cases = 0;
   int passedCases = 0, failedCases = 0;
   for (int i = 0; i < N; i++) {
-    // if (i != 1) continue;
-
     ChessBoard& sBoard = testBoards[i];
 
     /* may want to print board only for failed cases to reduce clutter */
@@ -157,13 +48,8 @@ void testMoveGeneration(std::function<Moves(Board*, Side)> generatingFunction,
     toBitboard(sBoard, bbs);
     int wKing = __builtin_ctzll(bbs[pieceIndex['K']]), bKing = __builtin_ctzll(bbs[pieceIndex['k']]);
 
-    // std::cout << "White king location: " << wKing << " or " << stringify(wKing) 
-    //           << "\nBlack king location: " << bKing << " or " << stringify(bKing) << '\n';
-
-    Board wBoard = Board(bbs, castlingPrivileges[i], enpassantWhite[i], bKing, wKing,
-                              kingInCheckWhite[i], kingInCheckBlack[i], whiteAttackers[i], blackAttackers[i]);
-    Board bBoard = Board(bbs, castlingPrivileges[i], enpassantBlack[i], bKing, wKing,
-                              kingInCheckWhite[i], kingInCheckBlack[i], whiteAttackers[i], blackAttackers[i]);
+    Board wBoard = Board(bbs, castlingPrivileges[i], enpassantWhite[i], bKing, wKing);
+    Board bBoard = Board(bbs, castlingPrivileges[i], enpassantBlack[i], bKing, wKing);
       
     std::cout << "Testing white... ";
     if (testingWhite[i] && ++cases) {
@@ -177,7 +63,7 @@ void testMoveGeneration(std::function<Moves(Board*, Side)> generatingFunction,
         std::cout << "\nExpected:\n";
         printMoves(expectedWhite[i]);
         std::cout << std::endl;
-        // << '\nExpected:\n' << printMoves(expectedWhite) << std::endl;
+        // std::cout << '\nExpected:\n' << printMoves(expectedWhite) << std::endl;
         ++failedCases;
       }
     } else {
@@ -196,7 +82,7 @@ void testMoveGeneration(std::function<Moves(Board*, Side)> generatingFunction,
         std::cout << "\nExpected:\n";
         printMoves(expectedBlack[i]);
         std::cout << std::endl;
-        // << '\nExpected:\n' << printMoves(expectedBlack) << std::endl;
+        // std::cout << '\nExpected:\n' << printMoves(expectedBlack) << std::endl;
         ++failedCases;
       }
     } else {
@@ -204,9 +90,9 @@ void testMoveGeneration(std::function<Moves(Board*, Side)> generatingFunction,
     }
   }
   if (!failedCases)
-    std::cout << "ALL PASSED!" << std::endl;
+    std::cout << "ALL PASSED!\n\n";
   else
-    std::cout << "PASSED " << passedCases << '/' << cases << " cases.\n";
+    std::cout << "PASSED " << passedCases << '/' << cases << " cases.\n\n";
 }
 
 void testAttackGeneration() {
@@ -216,11 +102,11 @@ void testAttackGeneration() {
   auto knightAttacks = compileKnightAttacks();
   printAttacks(knightAttacks); // works
 
-  auto slidingAttacks = compileSlidingAttacks(); // works
+  auto board = Board();
   std::array<ull, 64> directionBM;
-  for (int dir = 6; dir < 7; dir++) {
+  for (int dir : {WEST, EAST, NORTH, SOUTH, NORTHEAST, SOUTHWEST, NORTHWEST, SOUTHEAST}) {
     for (int i = 0; i < 64; i++) 
-      directionBM[i] = slidingAttacks[i][dir];
+      directionBM[i] = board.slidingAttacks()[i][dir];
     printAttacks(directionBM);
     std::cout << '\n';
   }
@@ -238,11 +124,7 @@ void testKingMoveGeneration() {
     TESTING_BLACK,
     ENPASSANT_WHITE,
     ENPASSANT_BLACK,
-    CASTLING_PRIVILEGES,
-    KING_IN_CHECK_WHITE,
-    KING_IN_CHECK_BLACK,
-    WHITE_ATTACKERS,
-    BLACK_ATTACKERS
+    CASTLING_PRIVILEGES
   );
 }
 
@@ -258,13 +140,8 @@ void testPawnMoveGeneration() {
     TESTING_BLACK,
     ENPASSANT_WHITE,
     ENPASSANT_BLACK,
-    CASTLING_PRIVILEGES,
-    KING_IN_CHECK_WHITE,
-    KING_IN_CHECK_BLACK,
-    WHITE_ATTACKERS,
-    BLACK_ATTACKERS
+    CASTLING_PRIVILEGES
   );
-
 }
 
 void testBishopMoveGeneration() {
@@ -279,13 +156,8 @@ void testBishopMoveGeneration() {
     TESTING_BLACK,
     ENPASSANT_WHITE,
     ENPASSANT_BLACK,
-    CASTLING_PRIVILEGES,
-    KING_IN_CHECK_WHITE,
-    KING_IN_CHECK_BLACK,
-    WHITE_ATTACKERS,
-    BLACK_ATTACKERS
+    CASTLING_PRIVILEGES
   );
-
 }
 
 void testKnightMoveGeneration() {
@@ -300,13 +172,8 @@ void testKnightMoveGeneration() {
     TESTING_BLACK,
     ENPASSANT_WHITE,
     ENPASSANT_BLACK,
-    CASTLING_PRIVILEGES, 
-    KING_IN_CHECK_WHITE,
-    KING_IN_CHECK_BLACK,
-    WHITE_ATTACKERS,
-    BLACK_ATTACKERS
+    CASTLING_PRIVILEGES
   );
-
 } 
 
 void testQueenMoveGeneration() {
@@ -321,13 +188,8 @@ void testQueenMoveGeneration() {
     TESTING_BLACK,
     ENPASSANT_WHITE,
     ENPASSANT_BLACK,
-    CASTLING_PRIVILEGES,
-    KING_IN_CHECK_WHITE,
-    KING_IN_CHECK_BLACK,
-    WHITE_ATTACKERS,
-    BLACK_ATTACKERS
+    CASTLING_PRIVILEGES
   );
-
 }
 
 void testRookMoveGeneration() {
@@ -342,13 +204,8 @@ void testRookMoveGeneration() {
     TESTING_BLACK,
     ENPASSANT_WHITE,
     ENPASSANT_BLACK,
-    CASTLING_PRIVILEGES,
-    KING_IN_CHECK_WHITE,
-    KING_IN_CHECK_BLACK,
-    WHITE_ATTACKERS,
-    BLACK_ATTACKERS
+    CASTLING_PRIVILEGES
   );
-
 }
 
 void testEvaluation() {
@@ -358,11 +215,7 @@ void testEvaluation() {
     eval.evaluate, 
     TEST_BOARDS, 
     ENPASSANT,
-    CASTLING_PRIVILEGES, 
-    KING_IN_CHECK_WHITE, 
-    KING_IN_CHECK_BLACK, 
-    WHITE_ATTACKERS, 
-    BLACK_ATTACKERS
+    CASTLING_PRIVILEGES
   );
 }
 
@@ -370,12 +223,7 @@ template<std::size_t N>
 void testEvaluator(std::function<Evaluation::Score(const Board&)> evaluatingFunction,
                     std::array<ChessBoard, N> testBoards,
                     std::array<std::optional<int>, N> enpassant,
-                    std::array<std::vector<char>, N> castlingPrivileges,
-                    std::array<bool, N> kingInCheckWhite,
-                    std::array<bool, N> kingInCheckBlack,
-                    std::array<ull, N> whiteAttackers,
-                    std::array<ull, N> blackAttackers) {
-
+                    std::array<std::vector<char>, N> castlingPrivileges) {
   std::function<Evaluation::Score(const Board&)> evalFunc = evaluatingFunction;
   for (int i = 0; i < N; i++) {
     ChessBoard& sBoard = testBoards[i];
@@ -384,8 +232,7 @@ void testEvaluator(std::function<Evaluation::Score(const Board&)> evaluatingFunc
     toBitboard(sBoard, bbs);
     int wKing = __builtin_ctzll(bbs[pieceIndex['K']]), bKing = __builtin_ctzll(bbs[pieceIndex['k']]);
 
-    Board board = Board(bbs, castlingPrivileges[i], enpassant[i], bKing, wKing,
-                              kingInCheckWhite[i], kingInCheckBlack[i], whiteAttackers[i], blackAttackers[i]);
+    Board board = Board(bbs, castlingPrivileges[i], enpassant[i], bKing, wKing);
 
     std::cout << "Evaluating board:\n";
     printStringifiedBoard(sBoard);
