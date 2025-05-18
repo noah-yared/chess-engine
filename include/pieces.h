299835c38bbe23c78142ce705ea5cdfe25628a7c
Noah Yared <noahy8105@gmail.com>
2025-05-18 09:16:28 -0400

    added NONE piece type

--- include/pieces.h
+++ include/pieces.h
@@ -2,7 +2,7 @@
 #define PIECES_H
 
 namespace Pieces {
-enum piece { p, r, n, b, q, k, P, R, N, B, Q, K };
+enum piece { p, r, n, b, q, k, P, R, N, B, Q, K, NONE };
 enum type { PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING };
 }
 
